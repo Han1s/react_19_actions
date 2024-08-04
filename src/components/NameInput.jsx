c5a51194fc3543bc3822c77ba4cb865d0e60c733
Jan Ranostaj <HonzaRanostaj@gmail.com>
2024-08-04 10:41:01 +0200

    3_useActionState

--- src/components/NameInput.jsx
+++ src/components/NameInput.jsx
@@ -1,47 +1,39 @@
-import { useState, useTransition } from "react";
+import { useActionState, useState } from "react";
 import { updateName } from "./actions";
 
 const NameInput = () => {
   const [name, setName] = useState("");
-  const [inputValue, setInputValue] = useState("");
-  const [error, setError] = useState(null);
-
-  const [isPending, startTransition] = useTransition();
-
-  const handleSubmit = async () => {
-    setError(null);
-
-    startTransition(async () => {
+  const [error, submitAction, isPending] = useActionState(
+    async (_previousReturnValue, formData) => {
       try {
-        const updatedName = await updateName(inputValue);
+        const updatedName = await updateName(formData.get("name"));
         setName(updatedName);
+        return null;
       } catch (error) {
-        setError(error);
+        return error;
       }
-    });
-  };
+    }
+  );
 
   return (
-    <form className="flex flex-col gap-4 w-96 m-auto">
+    <form className="flex flex-col gap-4 w-96 m-auto" action={submitAction}>
       <label className="text-gray-700" htmlFor="name">
         Your Name
       </label>
       <input
-        onChange={(event) => setInputValue(event.target.value)}
         type="text"
         id="name"
         name="name"
         className="border rounded py-2 px-3"
       />
       <button
-        onClick={handleSubmit}
-        type="button"
+        type="submit"
         disabled={isPending}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 rounded"
       >
         {isPending ? "Loading..." : "Update"}
       </button>
-      {error && <p className="text-red-500">Error updating form!</p>}
+      {error && <p className="text-red-500">{error}</p>}
       <p>Updated name: {name}</p>
     </form>
   );
